41cd5d5f025d1b7853552f6b28064baef3e26a74
EmmaTwersky <EmmaTwersky@10.25.10.80>
2017-03-06 11:04:56 -0800

    Add new "notebar" class

---
+++ src/tunecomposer/NoteBar.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tunecomposer;
+
+/**
+ *
+ * @author EmmaTwersky
+ */
+public class NoteBar {
+    public String instrumentName;
+    public int instrumentNumber;
+    public int pitch;
+    
+    public int startTick;
+    public int length;
+    
+    public float xValue;
+    public float yValue;
+    
+    public void addNote(){
+        
+    }
+    
+    public void editNote(){
+        
+    }
+}
