1ee37659e3eda97ab8e2758656b91c867c06c94d
EmmaTwersky <EmmaTwersky@192.168.1.4>
2017-03-11 13:27:21 -0800

    re-factor and separate classes

---
+++ src/tunecomposer/MenuController.java
@@ -0,0 +1,82 @@
+package tunecomposer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+
+/**
+ * This class controls the menu bar and menu bar actions.
+ * 
+ * @author Emma
+ */
+public class MenuController implements Initializable{    
+    /**
+     * Initialize FXML, draws initial setup of composition pane and 
+     * initialized the RedBar.
+     * 
+     * @param location the source of the scene
+     * @param resources the resources of the utility of the scene
+     * @see RedBar
+     */
+    @FXML
+    @Override
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+ 
+    }
+    
+    /**
+     * Handles the play button from the Actions menu.
+     * 
+     * @param event the menu selection event
+     */
+    @FXML 
+    protected void handlePlayScaleButtonAction(ActionEvent event) {
+        CompositionPaneController.tunePlayerObj.play();
+    }
+    
+    /**
+     * Handles the Stop menu item.
+     * 
+     * @param event the button click event
+     */
+    @FXML 
+    protected void handleStopPlayingButtonAction(ActionEvent event) {
+        CompositionPaneController.tunePlayerObj.stop();
+    }     
+    
+    /**
+     * Handles the Select All menu item and selects all notes.
+     * 
+     * @param event the button click event
+     */
+    @FXML
+    protected void handleSelectAllMenuItemAction(ActionEvent event) {
+        TunePlayer.MUSIC_NOTES_ARRAY.forEach((note) -> {
+            note.selectNote();
+        });
+        TunePlayer.updateSelectedNotesArray();
+    }
+
+    /**
+     * Handles the Delete menu item and deletes all selected notes.
+     * 
+     * @param event the button click event
+     */
+    @FXML
+    protected void handleDeleteMenuItemAction(ActionEvent event) {
+        for (NoteBar note: TunePlayer.SELECTED_NOTES_ARRAY) {
+            note.deleteNote();
+            TunePlayer.MUSIC_NOTES_ARRAY.remove(note);
+        }
+        TunePlayer.updateSelectedNotesArray();
+    }
+
+    /**
+     * Handles the Exit menu item and exits the scene.
+     * 
+     * @param event the menu selection event
+     */
+    @FXML
+    protected void handleExitMenuItemAction(ActionEvent event) {
+        System.exit(0);
+    }
+}
