accd80c9cc3ab7185eb24219c7af81c5dca39718
EmmaTwersky <EmmaTwersky@192.168.1.4>
2017-03-11 13:31:48 -0800

    re-factor and separate classes

--- src/tunecomposer/InstrumentSelection.java
+++ src/tunecomposer/InstrumentSelection.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 /**
  * This class creates and stores HashMaps to access MidiPlayer value constants.
+ * 
  * @author EmmaTwersky
  */
 public class InstrumentSelection {
@@ -14,26 +15,33 @@ public class InstrumentSelection {
      */
     private static final int PIANO = 0; //1-8
     private static final int PIANO_CHANNEL = 0;
+    
     private static final int HARPSICHORD = 7; //7
     private static final int HARPSICHORD_CHANNEL = 1;
+    
     private static final int MARIMBA = 13; //13
     private static final int MARIMBA_CHANNEL = 2;
+    
     private static final int ORGAN = 17; //17-24
     private static final int ORGAN_CHANNEL = 3;
+    
     private static final int ACCORDIAN = 22; //22
     private static final int ACCORDIAN_CHANNEL = 4;
+    
     private static final int GUITAR = 25; //25-32
     private static final int GUITAR_CHANNEL = 5;
+    
     private static final int VIOLIN = 41; //41
     private static final int VIOLIN_CHANNEL = 6;
+    
     private static final int FRENCHHORN = 61; //61
     private static final int FRENCHHORN_CHANNEL = 7;
     
     /**
      * Initializes the HashMaps to store the value and channel information.
      */
-    public HashMap<String, Integer> INSTRUMENT_VALUES = new HashMap<String, Integer>(8);
-    public HashMap<String, Integer> INSTRUMENT_CHANNELS = new HashMap<String, Integer>(8);
+    public HashMap<String, Integer> INSTRUMENT_VALUES = new HashMap();
+    public HashMap<String, Integer> INSTRUMENT_CHANNELS = new HashMap();
 
     /**
      * Constructs the value and channel HashMaps.
