64f2bb2da1bfd690a3f32bff4b526909e638d889
EmmaTwersky <EmmaTwersky@192.168.1.4>
2017-04-13 13:50:26 -0700

    Just trying to refactor all this and see if it can happen

---
+++ src/tunecomposer/SelectionWindowPaneController.java
@@ -0,0 +1,63 @@
+package tunecomposer;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class SelectionWindowPaneController implements Initializable {
+    /**
+     * Initialize a Rectangle window to select notes on dragged.
+     */
+    @FXML
+    public Pane selectionWindowPane;
+            
+    @FXML
+    public Rectangle SELECTION_WINDOW;
+    
+    /**
+     * The coordinates of the selection window to be manipulated when dragged.
+     */
+    public double topX;
+    public double topY;
+    public double width;
+    public double height;
+    public double dragStartX;
+    public double dragStartY;
+    
+    @FXML
+    @Override
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+        SELECTION_WINDOW.setVisible(false);
+    }
+    
+    public void translateWindow(double x, double y) {   
+//        topX = SELECTION_WINDOW.getX();
+//        topY = SELECTION_WINDOW.getY();
+        
+        if ((x > dragStartX) && (y > dragStartY)) {
+            width = x - dragStartX;
+            height = y - dragStartY;
+        }
+        else if ((x > dragStartX) && (y < dragStartY)) {
+            width = x - dragStartX;
+            height = dragStartY - y;
+            topY = y;
+        }
+        else if ((x < dragStartX) && (y > dragStartY)) {
+            width = dragStartX - x;
+            height = y - dragStartY;
+            topX = x;
+        }
+        else {
+            width = dragStartX - x;
+            height = dragStartY - y;
+            topX = x;
+            topY = y;
+        }
+        SELECTION_WINDOW.setX(topX);
+        SELECTION_WINDOW.setY(topY);
+        SELECTION_WINDOW.setWidth(width);
+        SELECTION_WINDOW.setHeight(height);
+    }
+}
