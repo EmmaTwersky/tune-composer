32ccd7770362b85b53007163798f0aa9cc57f4be
lazarcl <lazarcl@whitman.edu>
2017-04-16 23:24:50 -0700

    clarify javadoc and refactor methods

--- src/tunecomposer/TunePlayer.java
+++ src/tunecomposer/TunePlayer.java
@@ -1,32 +1,30 @@
 package tunecomposer;
 
-import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 
 /**
  * This class stores and controls the notes and creates the MidiPlayer to 
  * manipulate the tune.
- *
- * @author EmmaTwersky
  */
-public class TunePlayer {        
+public class TunePlayer {    
+    
     /**
      * One midi player is used throughout, so that it can be stopped.
      * Set resolution to 100 and beats per minute to 60.
      */
-    private final int resolution = 100;
-    private final int beatsPerMinute = 60;
+    private static final int RESOLUTION = 100;
+    private static final int BEATS_PER_MINUTE = 60;
     private final MidiPlayer player;
     
     TunePlayer() {
-        this.player = new MidiPlayer(resolution, beatsPerMinute);
+        this.player = new MidiPlayer(RESOLUTION, BEATS_PER_MINUTE);
     }
     
     /**
-     * Plays the sequence and begins the RedBar animation.
+     * Plays the SoundObjects on the given pane.
      * 
-     * @param soundObjectPane
+     * @param soundObjectPane pane the SoundObjects are kept in
      */
     protected void play(Pane soundObjectPane) {
         playSequence(soundObjectPane);
@@ -34,10 +32,10 @@ public class TunePlayer {
     
     /**
      * Plays the MidiPLayer.
-     * Each time playSequence is called the sequence clears, adds all notes 
-     * to the player, and starts from the beginning.
+     * Each time playSequence is called the current sequence clears, all SoundObjects 
+     * in given pane are added to the player, and player starts from the beginning.
      * 
-     * @param soundObjectPane
+     * @param soundObjectPane pane that contains the SoundObjects
      */
     protected void playSequence(Pane soundObjectPane) {
         player.stop();
@@ -51,12 +49,14 @@ public class TunePlayer {
      */
     private void populateMidiPlayer(Pane soundObjectPane) {
         soundObjectPane.getChildren().forEach((sObj) -> {
-            ((SoundObject) ((Rectangle) sObj).getUserData()).addToMidiPlayer(player);
+            Rectangle r = (Rectangle) sObj;
+            SoundObject s = (SoundObject) r.getUserData();
+            s.addToMidiPlayer(player);
         });
     }
     
     /**
-     * Stops the current MidiPlayer, clears MidiPlayer and stops RedBar.
+     * Stops the current MidiPlayer, clears MidiPlayer.
      */
     public void stop() {
         player.stop();
